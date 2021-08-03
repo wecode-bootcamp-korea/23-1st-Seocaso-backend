3b1e6bd88ce5c1923e84aa8a3254069119a89f63
jun <wkdghwns91@gmail.com>
2021-08-03 16:30:59 +0900

    Modify: image_url null=True 속성 추가

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     email     = models.CharField(max_length=100)
     password  = models.CharField(max_length=500)
     nickname  = models.CharField(max_length=45)
-    image_url = models.URLField(max_length=1000)
+    image_url = models.URLField(max_length=1000, null=True)
 
     class Meta:
         db_table = 'users'
