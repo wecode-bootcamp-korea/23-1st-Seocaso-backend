b2daafbc6b553dc0dc1409ce93c45e00eb75b087
jun <wkdghwns91@gmail.com>
2021-08-04 15:26:36 +0900

    wip

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
 
-# Create your views here.
+from cafes.models import Cafe
+from likes.models import CafeLike
+
+class CafeLikeView(View):
+    @LoginConfirm
+    def post(self, request, cafe_id):
+        if not CafeLike.objects.filter(cafe_id=cafe_id, user_id=request.user.id):
+            CafeLike.objects.create(
+                user = request.user,
+                cafe = Cafe.objects.get(id=cafe_id)
+            )
+
+            return JsonResponse({'MESSAGE' : 'CREATED'}, status=200)
+
+        else:
+            CafeLike.objects.filter(cafe_id=cafe_id, user_id=request.user.id).delete()
+
+            return JsonResponse({'MESSAGE' : 'DELETED'}, status=200)
+    
