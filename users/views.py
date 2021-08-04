aee6ec88d8df0c1cc96323455b2fc8597ad376a5
jun <wkdghwns91@gmail.com>
2021-08-04 14:16:10 +0900

    Modify: validation 변수명 수정 algorithm my_settings.py에 넣어서 안보이게 수정 로그인 시 해당 user가 db에 없을시에 에러 리턴

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.http  import JsonResponse
 from django.views import View
 
 from users.models import User
-from my_settings  import SECRET_KEY
+from my_settings  import SECRET_KEY, algorithm
 
 class SignupView(View):
     def post(self, request):
@@ -13,17 +13,17 @@ class SignupView(View):
             if User.objects.filter(email=data['email']).exists():
                 return JsonResponse({'message':'EMAIL_ALREADY_EXIST'}, status=400)
 
-            name_type     = re.compile('[\S]{2,}')
-            email_type    = re.compile('[a-z0-9-_.]+@[a-z]+\.[a-z]')
-            password_type = re.compile('(?=.{10,})(?=.*[a-zA-Z!@#$%^&*()_+~])(?=.*[!@#$%^&*()_+~0-9]).*')
+            NAME_VALIDATION     = re.compile('[\S]{2,}')
+            EMAIL_VALIDATION    = re.compile('[a-z0-9-_.]+@[a-z]+\.[a-z]')
+            PASSWORD_VALIDATION = re.compile('(?=.{10,})(?=.*[a-zA-Z!@#$%^&*()_+~])(?=.*[!@#$%^&*()_+~0-9]).*')
             
-            if not name_type.match(data['nickname']):
+            if not NAME_VALIDATION.match(data['nickname']):
                 return JsonResponse({'message':'NAME_FORMAT_ERROR'}, status=401)
             
-            if not email_type.match(data['email']):
+            if not EMAIL_VALIDATION.match(data['email']):
                 return JsonResponse({'message':'EMAIL_FORMAT_ERROR'}, status=401)
             
-            if not password_type.match(data['password']):
+            if not PASSWORD_VALIDATION.match(data['password']):
                 return JsonResponse({'message':'PASSWORD_FORMAT_ERROR'}, status=401)
 
             hashed_password = bcrypt.hashpw(
@@ -49,18 +49,22 @@ class SigninView(View):
             data     = json.loads(request.body)
             email    = data['email']
             password = data['password']
+            user     = User.objects.get(email=email)
 
             if not User.objects.filter(email=email).exists():
                 return JsonResponse({'MESSAGE' : "EMAIL DOESN'T EXIST"}, status=400)
             
-            if not bcrypt.checkpw( password.encode('utf-8'), User.objects.get(email=email).password.encode('utf-8') ):
+            if not bcrypt.checkpw( password.encode('utf-8'), user.password.encode('utf-8') ):
                 return JsonResponse({'MESSAGE' : 'WRONG PASSWORD'}, status=401)
             
-            access_token = jwt.encode({'id' : User.objects.get(email=email).id}, SECRET_KEY, algorithm='HS256')
+            access_token = jwt.encode({'id' : user.id}, SECRET_KEY, algorithm=algorithm)
             return JsonResponse({'TOKEN' : access_token}, status=200)
 
         except KeyError:
             return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status=400)
 
         except json.decoder.JSONDecodeError:
-            return JsonResponse({'message':'JSONDecodeError'}, status=400)
+            return JsonResponse({'MESSAGE':'JSONDecodeError'}, status=400)
+
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE' : 'USER_DOES_NOT_EXIST'}, status=400)
