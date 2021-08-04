4615d95dff3d33983697cc72e02a61c13e1fde10
jun <wkdghwns91@gmail.com>
2021-08-04 21:05:53 +0900

    Add: 리뷰 좋아요 기능 추가

--- likes/urls.py
+++ likes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from likes.views import CafeLikeView
+from likes.views import CafeLikeView, ReviewLikeView
 
 urlpatterns = [
-    path('/<int:cafe_id>/cafe', CafeLikeView.as_view())
+    path('/<int:cafe_id>/cafe', CafeLikeView.as_view()),
+    path('/<int:review_id>/review', ReviewLikeView.as_view())
 ]
--- likes/views.py
+++ likes/views.py
@@ -2,7 +2,7 @@ from django.http  import JsonResponse
 from django.views import View
 
 from cafes.models   import Cafe
-from likes.models   import CafeLike
+from likes.models   import CafeLike, ReviewLike
 from reviews.models import Review
 from utils          import LoginConfirm
 
@@ -17,16 +17,26 @@ class CafeLikeView(View):
                 user = request.user,
                 cafe = Cafe.objects.get(id=cafe_id)
             )
-
             return JsonResponse({'MESSAGE' : 'LIKE_CREATED'}, status=200)
 
         else:
             CafeLike.objects.filter(cafe_id=cafe_id, user_id=request.user.id).delete()
-
             return JsonResponse({'MESSAGE' : 'LIKE_DELETED'}, status=200)
 
-# class ReviewLikeView(View):
-#     @LoginConfirm
-#     def post(self, request, cafe_id):
-#         if not Cafe.objects.filter()
+class ReviewLikeView(View):
+    @LoginConfirm
+    def post(self, request, review_id):
+        if not Review.objects.filter(id=review_id):
+            return JsonResponse({'MESSAGE' : 'REVIEW_DOES_NOT_EXIST'}, status=400)
+
+        if not ReviewLike.objects.filter(id=review_id, user_id=request.user.id):
+            ReviewLike.objects.create(
+                user   = request.user,
+                review = Review.objects.get(id=review_id)
+            )
+            return JsonResponse({'MESSAGE' : 'LIKE_CREATED'}, status=200)
+
+        else:
+            ReviewLike.objects.filter(id=review_id, user_id=request.user.id).delete()
+            return JsonResponse({'MESSAGE' : 'LIKE_DELETED'}, status=200)
     
