b0e31274a4d35000f566b95e842c722fd9fce060
jun <wkdghwns91@gmail.com>
2021-08-04 18:26:59 +0900

    Modify: algorithm my_settings.py안에 작성하고 import, 컨벤션

--- utils.py
+++ utils.py
@@ -1,14 +1,14 @@
 import jwt
 
-from django.http import JsonResponse
+from django.http  import JsonResponse
 
 from users.models import User
-from my_settings import SECRET_KEY
+from my_settings  import SECRET_KEY, ALGORITHM
 
 def LoginConfirm(func):
     def wrapper(self, request, *args, **kwargs):
         user_token = request.headers.get('Authorization')
-        header = jwt.decode(user_token, SECRET_KEY, algorithms='HS256')
+        header     = jwt.decode(user_token, SECRET_KEY, ALGORITHM)
 
         if not User.objects.filter(id=header['id']).exists():
             return JsonResponse({'MESSAGE' : 'INVALID_USER'}, status=401)
