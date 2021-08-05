586647bc81ac4c657d7e83e7960a0849391d8667
jun <wkdghwns91@gmail.com>
2021-08-05 15:59:49 +0900

    Modify: get_or_create 사용하여 코드 수정

--- likes/views.py
+++ likes/views.py
@@ -14,34 +14,14 @@ class CafeLikeView(View):
         if not Cafe.objects.filter(id=cafe_id).exists:
             return JsonResponse({'MESSAGE' : 'CAFE_DOES_NOT_EXIST'}, status=400)
 
-        like, flag = CafeLike.objects.get_or_create(
-            cafe = Cafe.objects.get(id=cafe_id)
-        )
+        like, flag = CafeLike.objects.get_or_create(cafe=Cafe.objects.get(id=cafe_id), user=request.user)
 
         if not flag:
             like.delete()
             return JsonResponse({'MESSAGE' : 'LIKE_DELETED'}, status=200)
 
         else:
-            CafeLike.objects.create(
-                user = request.user,
-                cafe = Cafe.objects.get(id=cafe_id)
-            )
             return JsonResponse({'MESSAGE' : 'LIKE_CREATED'}, status=201)
-
-        # if not Cafe.objects.filter(id=cafe_id).exists():
-        #     return JsonResponse({'MESSAGE' : 'CAFE_NOT_FOUND'}, status=400)
-        
-        # if not CafeLike.objects.filter(cafe_id=cafe_id, user_id=request.user.id):
-        #     CafeLike.objects.create(
-        #         user = request.user,
-        #         cafe = Cafe.objects.get(id=cafe_id)
-        #     )
-        #     return JsonResponse({'MESSAGE' : 'CREATED'}, status=201)
-        
-        # else:
-        #     CafeLike.objects.filter(cafe_id=cafe_id, user_id=request.user.id).delete()
-        #     return JsonResponse({'MESSAGE' : 'DELETED'}, status=200)
 
 class ReviewLikeView(View):
     @log_in_confirm
@@ -49,14 +29,12 @@ class ReviewLikeView(View):
         if not Review.objects.filter(id=review_id).exists():
             return JsonResponse({'MESSAGE' : 'REVIEW_DOES_NOT_EXIST'}, status=400)
 
-        if not ReviewLike.objects.filter(review_id=review_id, user_id=request.user.id).exists():
-            ReviewLike.objects.create(
-                user   = request.user,
-                review = Review.objects.get(id=review_id)
-            )
-            return JsonResponse({'MESSAGE' : 'LIKE_CREATED'}, status=201)
+        like, flag = ReviewLike.objects.get_or_create(review=Review.objects.get(id=review_id), user=request.user)
 
-        else:
-            ReviewLike.objects.filter(review_id=review_id, user_id=request.user.id).delete()
+        if not flag:
+            like.delete()
             return JsonResponse({'MESSAGE' : 'LIKE_DELETED'}, status=200)
+        
+        else:
+            return JsonResponse({'MESSAGE' : 'LIKE_CRAETED'}, status=201)
     
