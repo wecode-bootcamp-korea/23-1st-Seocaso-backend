aba33eb5a04f3452b51acb72276bbda8435db264
jun <wkdghwns91@gmail.com>
2021-08-05 15:14:54 +0900

    Modify: jwt.decode 변수 header > user 변경, token이 조작된 경우 에러 리턴

--- utils.py
+++ utils.py
@@ -9,16 +9,19 @@ def log_in_confirm(func):
     def wrapper(self, request, *args, **kwargs):
         try:
             user_token = request.headers.get('Authorization')
-            header     = jwt.decode(user_token, SECRET_KEY, algorithms=ALGORITHM)
+            user       = jwt.decode(user_token, SECRET_KEY, algorithms=ALGORITHM)
 
-            if not User.objects.filter(id=header['id']).exists():
+            if not User.objects.filter(id=user['id']).exists():
                 return JsonResponse({'MESSAGE' : 'INVALID_USER'}, status=401)
 
-            request.user = User.objects.get(id=header['id'])
+            request.user = User.objects.get(id=user['id'])
 
             return func(self, request, *args, **kwargs)
 
         except jwt.InvalidSignatureError:
-            return JsonResponse({'MESSAGE' : 'INVALID_SIGNATURE_ERROR'}, status=400)
+            return JsonResponse({'MESSAGE' : 'JWT_SIGNATURE_ERROR'}, status=400)
+
+        except jwt.DecodeError:
+            return JsonResponse({'MESSAGE' : 'JWT_DECODE_ERROR'}, status=400)
 
     return wrapper
