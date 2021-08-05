912e6ccaf76be5c2b4a72d5c4e24704b1e2b21cf
choihyerim <hyerimc858@gmail.com>
2021-08-05 20:36:36 +0900

    Modity: url 수정

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from reviews.views import ReviewView, CommentOnReviewView
 
 urlpatterns = [
-    path('/<int:cafe_id>', ReviewView.as_view()),
+    path('/<int:cafe_id>/review', ReviewView.as_view()),
     path('/<int:review_id>/comment', CommentOnReviewView.as_view())
 ] 
