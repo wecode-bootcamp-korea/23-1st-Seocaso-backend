9fdca2d965c6e5aa0d66b4c242ec3aef55df2627
jun <wkdghwns91@gmail.com>
2021-08-05 12:52:11 +0900

    wip

--- utils.py
+++ utils.py
@@ -5,16 +5,16 @@ from django.http  import JsonResponse
 from users.models import User
 from my_settings  import SECRET_KEY, ALGORITHM
 
-def LoginConfirm(user_action):
+def log_in_confirm(func):
     def wrapper(self, request, *args, **kwargs):
         user_token = request.headers.get('Authorization')
-        header     = jwt.decode(user_token, SECRET_KEY, ALGORITHM)
+        header     = jwt.decode(user_token, SECRET_KEY, algorithms=ALGORITHM)
 
         if not User.objects.filter(id=header['id']).exists():
             return JsonResponse({'MESSAGE' : 'INVALID_USER'}, status=401)
 
         request.user = User.objects.get(id=header['id'])
 
-        return user_action(self, request, *args, **kwargs)
+        return func(self, request, *args, **kwargs)
 
     return wrapper
