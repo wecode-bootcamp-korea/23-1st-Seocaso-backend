57d7d8163d9ca6123a98f802cc392a06e6bbd6a1
jun <wkdghwns91@gmail.com>
2021-08-05 15:33:42 +0900

    wip

--- likes/views.py
+++ likes/views.py
@@ -5,6 +5,7 @@ from django.views import View
 from cafes.models   import Cafe
 from likes.models   import CafeLike, ReviewLike
 from reviews.models import Review
+from users.models import User
 from utils          import log_in_confirm
 
 class CafeLikeView(View):
@@ -13,7 +14,9 @@ class CafeLikeView(View):
         if not Cafe.objects.filter(id=cafe_id).exists:
             return JsonResponse({'MESSAGE' : 'CAFE_DOES_NOT_EXIST'}, status=400)
 
-        like, flag = CafeLike.objects.get_or_create()
+        like, flag = CafeLike.objects.get_or_create(
+            cafe = Cafe.objects.get(id=cafe_id)
+        )
 
         if not flag:
             like.delete()
@@ -22,7 +25,7 @@ class CafeLikeView(View):
         else:
             CafeLike.objects.create(
                 user = request.user,
-                cafe = Cafe.objects.filter(id=cafe_id)
+                cafe = Cafe.objects.get(id=cafe_id)
             )
             return JsonResponse({'MESSAGE' : 'LIKE_CREATED'}, status=201)
 
