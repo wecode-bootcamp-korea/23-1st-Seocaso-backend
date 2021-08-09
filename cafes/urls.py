dc286c51eb449db2b89d0ec1afa322120a2d2ebb
jun <wkdghwns91@gmail.com>
2021-08-10 01:10:26 +0900

    Add: 카페 별점 랭킹 기능

--- cafes/urls.py
+++ cafes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from cafes.views import ReviewView
+from cafes.views import ReviewView, RatingRankingView
 
 urlpatterns = [
-    path('/<int:cafe_id>/review', ReviewView.as_view())
+    path('/<int:cafe_id>/review', ReviewView.as_view()),
+    path('/rating-ranking', RatingRankingView.as_view())
 ]
--- cafes/views.py
+++ cafes/views.py
@@ -3,10 +3,12 @@ from json.decoder import JSONDecodeError
 
 from django.http.response import JsonResponse
 from django.views         import View
+from django.db.models     import Avg
 
 from reviews.models import Review
 from cafes.models   import Cafe
 from utils          import log_in_confirm
+from ratings.models import StarRating
 
 class ReviewView(View):
     @log_in_confirm
@@ -43,3 +45,37 @@ class ReviewView(View):
 
         review.delete()
         return JsonResponse({'MESSAGE' : 'REVIEW_DELETED'}, status=204)
+
+class RatingRankingView(View):
+    def get(self, request):
+        cafe_avg_rating        = StarRating.objects.values('cafe_id').annotate(avg_rating=Avg('score'))
+        rating_ranking         = list(cafe_avg_rating.order_by('-avg_rating'))
+        rating_ranking_results = []
+
+        if len(rating_ranking) > 10:
+            for i in range(10):
+                cafe_id    = rating_ranking[i]['cafe_id']
+                avg_rating = '{0:.2g}'.format(rating_ranking[i]['avg_rating'])
+
+                rating_ranking_results.append({
+                    'cafe_id'        : cafe_id,
+                    'cafe_name'      : Cafe.objects.get(id=cafe_id).name,
+                    'cafe_image'     : Cafe.objects.get(id=cafe_id).main_image_url,
+                    'cafe_address'   : Cafe.objects.get(id=cafe_id).address,
+                    'cafe_avg_rating': avg_rating
+                })
+        
+        else:
+            for i in range(cafe_avg_rating.count()):
+                cafe_id    = rating_ranking[i]['cafe_id']
+                avg_rating = str(rating_ranking[i]['avg_rating'])[:3]
+
+                rating_ranking_results.append({
+                    'cafe_id'        : cafe_id,
+                    'cafe_name'      : Cafe.objects.get(id=cafe_id).name,
+                    'cafe_image'     : Cafe.objects.get(id=cafe_id).main_image_url,
+                    'cafe_address'   : Cafe.objects.get(id=cafe_id).address,
+                    'cafe_avg_rating': avg_rating
+                })
+        
+        return JsonResponse({'RATING_RANKING_RESULTS' : rating_ranking_results})
