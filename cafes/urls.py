062b2fc61ff7882d27bd9962c738e308c1710cf5
jun <wkdghwns91@gmail.com>
2021-08-10 01:00:30 +0900

    Add: 카페 리뷰 랭킹 기능

--- cafes/urls.py
+++ cafes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from cafes.views import ReviewView
+from cafes.views import ReviewView, ReviewRankingView
 
 urlpatterns = [
-    path('/<int:cafe_id>/review', ReviewView.as_view())
+    path('/<int:cafe_id>/review', ReviewView.as_view()),
+    path('/review-ranking', ReviewRankingView.as_view())
 ]
