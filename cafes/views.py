a5763d1577b9f3f258ed6d4c2c5f638d39959ead
jun <wkdghwns91@gmail.com>
2021-08-10 17:23:16 +0900

    Modify: 컨벤션

--- cafes/views.py
+++ cafes/views.py
@@ -29,25 +29,27 @@ GROUP BY c.id;
 class CafeListView(View):
     def get(self, request):
         ordering = request.GET.get('ordering', None)
-        cafes = Cafe.objects.all().values('id').annotate(avg_rating=Avg('starrating__score')).order_by('-avg_rating')
-        results = []
-        cnt = 0
-        
+
+        cafes    = Cafe.objects.all().values('id').annotate(avg_rating=Avg('starrating__score')).order_by('-avg_rating')
+        results  = []
+        cnt      = 0
+
         if ordering == '-review_count':
             cafes_review_count = Cafe.objects.all().annotate(review_count=Count('review')).order_by('-review_count')
 
             for cafe in cafes_review_count:
                 if cnt == 10:
                     break
+                
                 cafe_id = cafe.id
-                this = Cafe.objects.get(id=cafe_id)
+                this    = Cafe.objects.get(id=cafe_id)
 
                 results.append({
-                    'id' : cafe_id,
-                    'name' : this.name,
-                    'address' : this.address,
-                    'image' : this.main_image_url,
-                    'avg_rating' : '%.1f' % cafes.get(id=cafe_id)['avg_rating']
+                    'id'        : cafe_id,
+                    'name'      : this.name,
+                    'address'   : this.address,
+                    'image'     : this.main_image_url,
+                    'avg_rating': '%.1f' % cafes.get(id=cafe_id)['avg_rating']
                 })
 
                 cnt += 1
@@ -59,7 +61,7 @@ class CafeListView(View):
                     break
 
                 cafe_id = cafe['id']
-                this = Cafe.objects.get(id=cafe_id)
+                this    = Cafe.objects.get(id=cafe_id)
 
                 results.append({
                     "id"        : cafe_id,
