cd3c644515041ad054852e00f37c7ed5a72a7b72
choihyerim <hyerimc858@gmail.com>
2021-08-10 14:39:27 +0900

    Add: 카페 평가 view 작성

--- cafes/urls.py
+++ cafes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from cafes.views import ReviewView, CommentOnReviewView
+from cafes.views import ReviewView, CommentOnReviewView, StarRatingView
 
 urlpatterns = [
     path('/<int:cafe_id>/review', ReviewView.as_view()),
-    path('/<int:review_id>/comment', CommentOnReviewView.as_view())
+    path('/<int:review_id>/comment', CommentOnReviewView.as_view()),
+    path('/<int:cafe_id>/star-rating', StarRatingView.as_view())
 ]
