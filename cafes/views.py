5a3de6777e3ec435b213f3c384a29fb8e6b82d30
choihyerim <hyerimc858@gmail.com>
2021-08-10 16:00:56 +0900

    Modify: 별점 기능 view 작성

--- cafes/views.py
+++ cafes/views.py
@@ -6,6 +6,7 @@ from django.views         import View
 
 from reviews.models import Review
 from cafes.models   import Cafe
+from ratings.models import StarRating
 from utils          import log_in_confirm
 
 class ReviewView(View):
@@ -62,3 +63,52 @@ class CommentOnReviewView(View):
     )
 
         return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+
+class StarRatingView(View):
+    @log_in_confirm
+    def post(self, request, cafe_id):
+        try:
+            data = json.loads(request.body)
+
+            user = request.user
+
+            if not Cafe.objects.filter(id=cafe_id).exists():
+                return JsonResponse({'MESSAGE':'CAFE_DOES_NOT_EXIST'}, status=401)
+
+            star, flag = StarRating.objects.get_or_create(cafe_id=cafe_id, user_id=user.id)
+
+            if not flag:
+                if star.score == data['score']:
+                    star.delete()
+                    return JsonResponse({'MESSAGE' : 'SCORE_DELETED'}, status=200)
+
+                else:
+                    star.score = data['score']
+                    star.save()
+                    return JsonResponse({'MESSAGE' : 'SCORE_UPDATED'}, status=200)
+            else:
+                StarRating.objects.filter(cafe_id=cafe_id, user_id=user.id).update(score=data['score'])
+                return JsonResponse({'MESSAGE' : 'SCORE_CREATED'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+
+        except JSONDecodeError:
+            return JsonResponse({'MESSAGE':'JSON_DECODE_ERROR'}, status=400)
+
+    def get(self, request, cafe_id):
+
+        star_ratings = StarRating.objects.filter(cafe_id=cafe_id)
+
+        results = []
+
+        for star_rating in star_ratings:
+            results.append(
+                {
+                    'score': star_rating.score,
+                    'cafe_id' : star_rating.cafe_id,
+                    'user_id' : star_rating.user_id
+                }
+            )
+
+        return JsonResponse({'result':results}, status=200)
