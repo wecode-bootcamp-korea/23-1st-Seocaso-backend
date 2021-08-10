93f2925d89774cf86e21d2e97fd453f811a01b02
choihyerim <hyerimc858@gmail.com>
2021-08-10 23:04:25 +0900

    Add: 검색 기능 구현

--- cafes/urls.py
+++ cafes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from cafes.views import ReviewView, CommentOnReviewView
+from cafes.views import ReviewView, CommentOnReviewView, SearchView
 
 urlpatterns = [
     path('/<int:cafe_id>/review', ReviewView.as_view()),
-    path('/<int:review_id>/comment', CommentOnReviewView.as_view())
+    path('/<int:review_id>/comment', CommentOnReviewView.as_view()),
+    path('', SearchView.as_view())
 ]
