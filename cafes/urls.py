373a8c392b8689e1c4cadd0a8fd0271efa489000
jun <wkdghwns91@gmail.com>
2021-08-11 20:54:25 +0900

    Fix: git error 수정

--- cafes/urls.py
+++ cafes/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from cafes.views import ReviewView, RatingCountView, CommentOnReviewView, CafeListView, MenuView
+from cafes.views import ReviewView, RatingCountView, CommentOnReviewView, CafeListView, MenuView, SearchView
 
 urlpatterns = [
     path('/<int:cafe_id>/review', ReviewView.as_view()),
     path('/rating-count', RatingCountView.as_view()),
     path('/<int:review_id>/comment', CommentOnReviewView.as_view()),
     path('', CafeListView.as_view()),
-    path('/<int:cafe_id>/menus', MenuView.as_view())
+    path('/<int:cafe_id>/menus', MenuView.as_view()),
+    path('', SearchView.as_view()),
 ]
